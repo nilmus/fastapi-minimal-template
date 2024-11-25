a2dc352b91ecfd437596df32aa2f1947f923259f
nilmus <nil_mustard_0e@icloud.com>
2024-11-25 22:59:48 +0100

    Update main.py
    remove a deprecated function

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ from config.database import create_db_and_tables  # , delete_db_and_tables
 from config.env import FRONTEND_SETTINGS
 from routers import token, user
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # delete_db_and_tables()
+    create_db_and_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(user.router)
 app.include_router(token.router)
-
-
-@app.on_event("startup")  # FIXME
-def on_startup():
-    # delete_db_and_tables()
-    create_db_and_tables()
 
 
 origins = [FRONTEND_SETTINGS.FRONTEND_URL]
